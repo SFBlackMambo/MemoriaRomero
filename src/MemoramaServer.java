72221892d31ae1150f4e759a3b5f885f89a3d837
sfblackmambo <betoboticario7@gmial.com>
2023-05-31 01:09:05 -0700

    ptmdes

--- src/MemoramaServer.java
+++ src/MemoramaServer.java
@@ -15,14 +15,17 @@ public class MemoramaServer {
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Servidor iniciado en el puerto " + port);
 
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Cliente conectado: " + clientSocket.getInetAddress().getHostAddress());
+            // Aceptar dos conexiones de clientes
+            Socket clientSocket1 = serverSocket.accept();
+            System.out.println("Cliente 1 conectado: " + clientSocket1.getInetAddress().getHostAddress());
+            Socket clientSocket2 = serverSocket.accept();
+            System.out.println("Cliente 2 conectado: " + clientSocket2.getInetAddress().getHostAddress());
 
-                // Crear una instancia de Runnable para manejar la comunicación con el cliente
-                Runnable clientHandler = new ClientHandler(clientSocket, "Jugador 1"); // Modificar los nombres de los jugadores según corresponda
-                executor.execute(clientHandler);
-            }
+            // Crear una instancia de Runnable para manejar la comunicación con cada cliente
+            Runnable clientHandler1 = new ClientHandler(clientSocket1, "Jugador 1");
+            executor.execute(clientHandler1);
+            Runnable clientHandler2 = new ClientHandler(clientSocket2, "Jugador 2");
+            executor.execute(clientHandler2);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
